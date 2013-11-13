f0ed13ae765c3c25501815679f8038e1a7779a06
Ram Raghunathan <ram.r@cs.cmu.edu>
2013-11-13 18:50:19 -0500

    Setup with my design. Only index.html worked on for now

--- site.hs
+++ site.hs
@@ -31,15 +31,11 @@ main = hakyllWith config $ do
         route   (constRoute "favicon.ico")
         compile copyFileCompiler
 
-    match "static/vCard.vcf" $ do
-        route   (constRoute "vCard.vcf")
-        compile copyFileCompiler
-
     match "static/*/*" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "scss/app.scss" $do
+    match ("scss/index.scss" .||. "scss/post.scss") $do
         route   $ gsubRoute "scss/" (const "static/css/") `composeRoutes` setExtension "css"
         compile $ getResourceString
             >>= withItemBody (unixFilter "sass" ["-s", "--scss", "--compass", "--style", "compressed"])
@@ -51,33 +47,15 @@ main = hakyllWith config $ do
         compile copyFileCompiler
 
     -- Compile static pages to web root with Pandoc
-    match (fromList ["cv.md"]) $ do
-        route   $ setExtension ""
-        compile $ pandocCompilerWith defaultHakyllReaderOptions pandocWriterOptions
-            >>= loadAndApplyTemplate "templates/generic.html" defaultContext
-            >>= loadAndApplyTemplate "templates/base.html" defaultContext
-            >>= relativizeUrls
-
     match "posts/*" $ do
         route $ setExtension ""
-        compile $ pandocCompilerWith defaultHakyllReaderOptions pandocWriterOptions
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/base.html" postCtx
-            >>= relativizeUrls
-
-    create ["archive"] $ do
-        route idRoute
         compile $ do
-            let archiveCtx =
-                    field "posts" (\_ -> itemList "posts/*" recentFirst)          `mappend`
-                    constField "title" "Posts archive"                   `mappend`
-                    constField "description" "Previous posts on this site"  `mappend`
-                    defaultContext
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/base.html" archiveCtx
-                >>= relativizeUrls
-
+          let baseCtx = constField "stylesheet" "static/css/post.css" `mappend`
+                        itemCtx
+          pandocCompilerWith defaultHakyllReaderOptions pandocWriterOptions
+            >>= loadAndApplyTemplate "templates/post.html"    itemCtx
+            >>= loadAndApplyTemplate "templates/base.html" baseCtx
+            >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
@@ -92,18 +70,20 @@ main = hakyllWith config $ do
                            "publications"
                            (\_ -> itemList "publications/*" $ fmap (take 3) . recentFirst)
 
+                baseCtx = constField "stylesheet" "static/css/index.css" `mappend`
+                          itemCtx
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/base.html" postCtx
+                >>= loadAndApplyTemplate "templates/base.html" baseCtx
                 >>= relativizeUrls
 
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+itemCtx :: Context String
+itemCtx =
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext
 
 
 --------------------------------------------------------------------------------
@@ -111,5 +91,5 @@ itemList :: Pattern -> ([Item String] -> Compiler [Item String]) -> Compiler Str
 itemList glob sortFilter = do
     posts   <- sortFilter =<< loadAll glob
     itemTpl <- loadBody "templates/post-item.html"
-    list    <- applyTemplateList itemTpl postCtx posts
+    list    <- applyTemplateList itemTpl itemCtx posts
     return list
