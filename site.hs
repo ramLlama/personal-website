ef8a9d1d4b6990763dc951d73da5aee5d2064064
Ram Raghunathan <ram.r@cs.cmu.edu>
2013-11-18 19:51:46 -0500

    Almost done!

--- site.hs
+++ site.hs
@@ -35,7 +35,9 @@ main = hakyllWith config $ do
         route   idRoute
         compile copyFileCompiler
 
-    match ("scss/index.scss" .||. "scss/post.scss") $do
+    match ("scss/index.scss" .||.
+           "scss/post.scss" .||.
+           "scss/item-index.scss") $ do
         route   $ gsubRoute "scss/" (const "static/css/") `composeRoutes` setExtension "css"
         compile $ getResourceString
             >>= withItemBody (unixFilter "sass" ["-s", "--scss", "--compass", "--style", "compressed"])
@@ -50,7 +52,7 @@ main = hakyllWith config $ do
     match "posts/*" $ do
         route $ setExtension ""
         compile $ do
-          let baseCtx = constField "stylesheet" "static/css/post.css" `mappend`
+          let baseCtx = constField "stylesheet" "/static/css/post.css" `mappend`
                         itemCtx
           pandocCompilerWith defaultHakyllReaderOptions pandocWriterOptions
             >>= loadAndApplyTemplate "templates/post.html"    itemCtx
@@ -62,21 +64,80 @@ main = hakyllWith config $ do
         compile $ do
             let indexCtx = field
                            "posts"
-                           (\_ -> itemList "posts/*" $ fmap (take 3) . recentFirst)
+                           (\_ -> postList $ fmap (take 3) . recentFirst)
 
                            `mappend`
 
                            field
                            "publications"
-                           (\_ -> itemList "publications/*" $ fmap (take 3) . recentFirst)
+                           (\_ -> publicationList $ fmap (take 3) . recentFirst)
 
-                baseCtx = constField "stylesheet" "static/css/index.css" `mappend`
+                baseCtx = constField "title" "Home" `mappend`
+                          constField "description" "CMU home page of Ram Raghunathan" `mappend`
+                          constField "stylesheet" "/static/css/index.css" `mappend`
                           itemCtx
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/base.html" baseCtx
                 >>= relativizeUrls
+
+    -- Create post and publication indices
+    create ["posts-index"] $ do
+      route (constRoute "posts/index.html")
+      compile $ do
+        let postIndexCtx =
+              constField "page-title" "Posts" `mappend`
+              -- field "items" (\_ -> postList recentFirst) `mappend`
+              constField "items" "Nothing yet!" `mappend`
+              defaultContext
+
+            baseCtx =
+              constField "title" "Posts" `mappend`
+              constField "description" "Previous posts on cs.cmu.edu/~rraghuna" `mappend`
+              constField "stylesheet" "/static/css/item-index.css" `mappend`
+              defaultContext
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/item-index.html" postIndexCtx
+          >>= loadAndApplyTemplate "templates/base.html" baseCtx
+          >>= relativizeUrls
+
+    create ["publications-index"] $ do
+      route (constRoute "publications/index.html")
+      compile $ do
+        let publicationIndexCtx =
+              constField "page-title" "Publications" `mappend`
+              -- field "items" (\_ -> publicationList recentFirst) `mappend`
+              constField "items" "Nothing yet!" `mappend`
+              defaultContext
+
+            baseCtx =
+              constField "title" "Publications" `mappend`
+              constField "description" "Publications findable on cs.cmu.edu/~rraghuna" `mappend`
+              constField "stylesheet" "/static/css/item-index.css" `mappend`
+              defaultContext
+        makeItem ""
+          >>= loadAndApplyTemplate "templates/item-index.html" publicationIndexCtx
+          >>= loadAndApplyTemplate "templates/base.html" baseCtx
+          >>= relativizeUrls
+
+    match "miscellaneous/index-items.html" $ do
+      route (constRoute "miscellaneous/index.html")
+      compile $ do
+        let miscellaneousIndexCtx =
+              constField "page-title" "Miscellaneous" `mappend`
+              bodyField "items" `mappend`
+              defaultContext
+
+            baseCtx =
+              constField "title" "Miscellaneous" `mappend`
+              constField "description" "Miscellaneous items on cs.cmu.edu/~rraghuna" `mappend`
+              constField "stylesheet" "/static/css/item-index.css" `mappend`
+              defaultContext
+        getResourceBody
+          >>= loadAndApplyTemplate "templates/item-index.html" miscellaneousIndexCtx
+          >>= loadAndApplyTemplate "templates/base.html" baseCtx
+          >>= relativizeUrls
 
 
 --------------------------------------------------------------------------------
@@ -87,9 +148,15 @@ itemCtx =
 
 
 --------------------------------------------------------------------------------
-itemList :: Pattern -> ([Item String] -> Compiler [Item String]) -> Compiler String
-itemList glob sortFilter = do
+itemList :: Pattern -> Identifier -> ([Item String] -> Compiler [Item String]) -> Compiler String
+itemList glob template sortFilter = do
     posts   <- sortFilter =<< loadAll glob
-    itemTpl <- loadBody "templates/post-item.html"
+    itemTpl <- loadBody template
     list    <- applyTemplateList itemTpl itemCtx posts
     return list
+
+postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
+postList sortFilter = itemList "posts/*" "templates/post-listing.html" sortFilter
+
+publicationList :: ([Item String] -> Compiler [Item String]) -> Compiler String
+publicationList sortFilter = itemList "publications/*" "templates/publication-listing.html" sortFilter
