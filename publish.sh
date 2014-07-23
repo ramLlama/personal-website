cb83f6409e075b4dea6859d4173fb27acff43c10
Ram Raghunathan <ram@llama.is>
2014-07-23 10:17:22 -0400

    Make publish.sh safer

--- publish.sh
+++ publish.sh
@@ -1,22 +1,28 @@
 #! /usr/bin/env bash
 
 # Do getoptions
-GETOPT=$(getopt --options="hv" --longoptions="help,verbose" -n "publish.sh" -- "$@")
+GETOPT=$(getopt --options="hvt:" --longoptions="help,verbose,target::" -n "publish.sh" -- "$@")
 if [[ $? != 0 ]]; then
     exit 1;
 fi
 
 eval set -- "$GETOPT"
 
-VERBOSE=0;
+VERBOSE=0
+TARGET=0
 while true; do
     case "$1" in
 	-h|--help)
-	    echo "./publish.sh <directory to publish to>"
+	    echo "./publish.sh [-v|--verbose] (-t|--target) <directory to publish to>"
 	    exit 0
 	    ;;
 	-v|--verbose)
 	    VERBOSE=1
+	    shift
+	    ;;
+	-t|--target)
+	    TARGET=$2
+	    shift
 	    shift
 	    ;;
 	--)
@@ -24,6 +30,7 @@ while true; do
 	    break
 	    ;;
 	*)
+	    echo $1
 	    shift
 	    echo "Internal Error!"
 	    exit 1;
@@ -31,7 +38,12 @@ while true; do
     esac
 done
 
-COMMAND="rsync --verbose --archive --delete --exclude='.htaccess' ./_site/ '$1'"
+if [[ $TARGET == 0 ]] ; then
+    echo "No target specified!" 1>&2
+    exit 1
+fi
+
+COMMAND="rsync --verbose --archive --delete --exclude='.htaccess' ./_site/ '$TARGET'"
 
 if [[ $VERBOSE != 0 ]]; then
     echo $COMMAND
