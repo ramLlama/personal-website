c7308fa7908d2374714f21bfdabc81eec361992e
Ram Raghunathan <ram.r@cs.cmu.edu>
2014-07-30 14:33:36 -0400

    set config.rb output path to match hakyll

--- config.rb
+++ config.rb
@@ -5,20 +5,19 @@ add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
+css_dir = "_site/static/css"
 sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
+line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
