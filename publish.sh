f45f0ddf16a6275a563ee0dc0fbcced3bedc8622
Ram Raghunathan <ram.r@cs.cmu.edu>
2014-09-03 17:00:00 -0400

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -1,34 +1,44 @@
 #! /usr/bin/env bash
 
 # Do getoptions
-GETOPT=$(getopt --options="hvt:" --longoptions="help,verbose,target::" -n "publish.sh" -- "$@")
+GETOPT=$(getopt --options="hvt:d" --longoptions="help,verbose,target:,dryrun" -n "publish.sh" -- "$@")
 if [[ $? != 0 ]]; then
     exit 1;
 fi
 
 eval set -- "$GETOPT"
 
-VERBOSE=0
+DRYRUN=0
 TARGET=0
+VERBOSE=0
 while true; do
     case "$1" in
 	-h|--help)
-	    echo "./publish.sh [-v|--verbose] (-t|--target) <directory to publish to>"
+	    echo "./publish.sh [-v|--verbose] {(-t|--target) <directory to publish to>}"
 	    exit 0
 	    ;;
+
 	-v|--verbose)
 	    VERBOSE=1
 	    shift
 	    ;;
+
 	-t|--target)
-	    TARGET=$2
+	    TARGET="$2"
 	    shift
 	    shift
 	    ;;
+
+	-d|--dryrun)
+	    DRYRUN=1
+	    shift
+	    ;;
+
 	--)
 	    shift
 	    break
 	    ;;
+
 	*)
 	    echo $1
 	    shift
@@ -49,4 +59,6 @@ if [[ $VERBOSE != 0 ]]; then
     echo $COMMAND
 fi
 
-eval exec "$COMMAND"
+if [[ $DRYRUN = 0 ]] ; then
+    eval exec "$COMMAND"
+fi
